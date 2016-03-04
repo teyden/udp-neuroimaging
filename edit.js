7425bbc42472efc3c181a14a694b3e722ec48bf1
danielpgross <daniel.gross.2@gmail.com>
2016-03-04 15:46:57 -0500

    stop using JSX

--- edit.js
+++ edit.js
@@ -1,37 +1,26 @@
-window.samplePatientRecordNeuroState = [
-	{
-		id: "hp:0002079",
-		date: "2015-01-20",
-		qualifiers: ["HP:0012837"],
-	},
-	{
-		id: "hp:0001338",
-		date: "2015-01-20",
-		qualifiers: ["HP:0012837"],
-	},
-	{
-		id: "hp:0008278",
-		date: "2015-10-01",
-	},
-	{
-		id: "hp:0001338",
-		date: "2016-01-01",
-		qualifiers: ["HP:0012837"],
-	},
-];
+window.samplePatientRecordNeuroState = [{
+	id: "hp:0002079",
+	date: "2015-01-20",
+	qualifiers: ["HP:0012837"]
+}, {
+	id: "hp:0001338",
+	date: "2015-01-20",
+	qualifiers: ["HP:0012837"]
+}, {
+	id: "hp:0008278",
+	date: "2015-10-01"
+}, {
+	id: "hp:0001338",
+	date: "2016-01-01",
+	qualifiers: ["HP:0012837"]
+}];
 
 fetch('config.json', {
 	method: 'get'
-})
-.then(function(response) {
+}).then(function (response) {
 	return response.json();
-})
-.then(function(json) {
+}).then(function (json) {
 	var rows = json;
 
-	ReactDOM.render(
-		<NeurologyTable config={rows} />,
-		document.getElementById('table-container')
-	);
+	ReactDOM.render(React.createElement(NeurologyTable, { config: rows }), document.getElementById('table-container'));
 });
-
